7c66096ca9723ded5c2a86cbc774143c52de0567
abhinav <abhinavgupta0148@gmail.com>
2025-04-26 13:54:28 -0400

    edit profile now returns true/false

--- db/functions/create_function_update_profile.sql
+++ db/functions/create_function_update_profile.sql
@@ -1,9 +1,12 @@
+-- updates user profile, if user id is valid -- returns true, else return false
 CREATE OR REPLACE FUNCTION update_profile(
     _user_id INTEGER,
     _full_name VARCHAR DEFAULT NULL,
     _bio TEXT DEFAULT NULL,
     _picture BYTEA DEFAULT NULL
-) RETURNS VOID AS $$
+) RETURNS BOOLEAN AS $$
+DECLARE
+    _updated_rows INTEGER;
 BEGIN
     UPDATE "user"
     SET
@@ -12,5 +15,13 @@ BEGIN
         picture = COALESCE(_picture, picture)
     WHERE id = _user_id
       AND terminated_at IS NULL;
+
+    GET DIAGNOSTICS _updated_rows = ROW_COUNT;
+
+    IF _updated_rows = 1 THEN
+        RETURN TRUE;  -- Update succeeded
+    ELSE
+        RETURN FALSE; -- No active user found or user is terminated
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
